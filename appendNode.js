578b21e0be50ab9b886922d262a568836634c138
Ethan Harry <ethan.m.harry@gmail.com>
2018-02-28 07:26:10 -0800

    Create appendNode.js

---
+++ appendNode.js
@@ -0,0 +1,12 @@
+var appendTarget = document.querySelector('#appendTarget');
+
+var node = document.createElement('h1');
+node.innerText = 'hey';
+
+console.log(appendTarget)
+console.log(node)
+
+
+setTimeout(() => {
+  appendTarget.appendChild(node);
+}, 5000)
